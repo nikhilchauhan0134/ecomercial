18b63cc7c73e2cc8f30ef374d6cc03e5ece2ebfd
rinkichauhan0134@gmail.com <rinkichauhan0134@gmail.com>
2020-02-16 08:26:19 +0530

    Add project files.

---
+++ ecomercial.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ecomercial.Web.Startup))]
+namespace ecomercial.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
